c77e4e300d1bbb26c020f380bdcafb049af84b51
caglar <eflatun126@gmail.com>
2022-03-11 01:07:39 +0300

    ...

--- lib/main2.dart
+++ lib/main2.dart
@@ -10,16 +10,37 @@ Future<void> main(List<String> args) async {
   );
   var dio = Dio(options);
 
-  await dio
+  logout(dio, "fed7c25d-49fc-4326-b2bf-f3f9729f634b");
+}
+
+Future<void> login(
+    Dio dio, String nickName, String email, String password) async {
+  await dio.post(
+      "https://notebook-b-api.herokuapp.com/api/authentication/login",
+      data: <String, dynamic>{
+        "userNickname": nickName,
+        "email": email,
+        "password": password
+      }).then((value) {
+    print(value.headers.toString() + " : headers");
+    print(value.statusCode.toString() + " status code");
+    print("gelen data : " + value.data.toString());
+  }).onError((error, stackTrace) {
+    print("gelen hata : " + error.toString());
+  });
+}
+
+Future<void> logout(Dio dio, String reftoken) async {
+  var response = await dio
       .post(
-          "https://notebook-b-api.herokuapp.com/api/authentication/login",data: <String,dynamic>{"userNickname":"a",
-    "email":null,
-    "password":"a"})
+          "https://notebook-b-api.herokuapp.com/api/authentication/logout?refreshToken=$reftoken")
       .then((value) {
     print(value.headers.toString() + " : headers");
     print(value.statusCode.toString() + " status code");
     print("gelen data : " + value.data.toString());
   }).onError((error, stackTrace) {
     print("gelen hata : " + error.toString());
+  }).catchError((e) {
+    print("yakalanan hata : " + e.toString());
   });
 }
