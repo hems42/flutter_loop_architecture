b60d5e51f0a6543b9e720078bea13db35c06df73
caglar <eflatun126@gmail.com>
2022-03-13 01:47:59 +0300

    exceptşon model için o kadar uğraştıydım hepsini silmişim yanlışlıkla tühhh...

---
+++ lib/core/extension/exception_types_extension.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_notebook/core/constants/enum/exception_error_types_enum.dart';
+
+import '../constants/enum/exception_event_types_enum.dart';
+
+extension ExceptionErrorTypesExtension on ExceptionErrorTypes {
+
+ // to error code
+ // to error title
+ //
+
+}
+
+extension ExceptEventTypesExtension on ExceptionEventTypes {
+
+  // to error code
+  // to error message
+  //
+}
---
+++ lib/core/init/exception/concrete/exception_models/UserNotFoundException.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_notebook/core/base/model/abstract/ife_base_exception_model.dart';
+
+class UserNotFoundException extends IBaseExceptionModel {
+  
+}
