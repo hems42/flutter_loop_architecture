65db3d404d94ff86fbf501c753d0c6dc8944ddeb
caglar <eflatun126@gmail.com>
2022-04-25 00:33:03 +0300

    .....

--- lib/view/authentication/_signup/viewmodel/signup_viewmodel.dart
+++ lib/view/authentication/_signup/viewmodel/signup_viewmodel.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook/view/authentication/_signup/model/signup_request_model.dart';
 import '../service/abstract/ife_signup_network_service.dart';
 import '../../../../core/base/model/abstract/ife_base_view_model.dart';
 import '../service/abstract/ife_singup_cache_service.dart';
 import 'package:mobx/mobx.dart';
+
 part 'signup_viewmodel.g.dart';
 
 class SingupViewModel = _SingupViewModelBase with _$SingupViewModel;
@@ -20,5 +24,9 @@ abstract class _SingupViewModelBase with Store, IBaseViewModel {
   @override
   void init() {
     // TODO: implement init
+  }
+
+  void singup(SignupRequestModel signupRequestModel) {
+    _signupService.signup(signupRequestModel);
   }
 }
